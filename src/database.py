1d1c53d40d14f405d9a23af7ccb8cdeacd014bbf
gitansh09 <44221749+gitansh09@users.noreply.github.com>
2025-12-03 10:38:32 +0530

    Create database.py

---
+++ src/database.py
@@ -0,0 +1,14 @@
+# src/database.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./weather.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}  # for SQLite + FastAPI
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
